31d1c2f4952757b97c347b8928f26829e484a5bd
Robert O'Steen <rosteen@jhu.edu>
2014-01-22 13:51:45 -0500

    Create access-reader.sh

---
+++ access-reader.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# The purpose of this script is to READ a log file --make it available to uesrs with out permission
+# AND make it HUMAN READABLE
+#
+#
+TD=`$DATE`
+UPTIME="`uptime`" # Determine how long the system has been running
+clear
+echo ""
+echo $UPTIME|
+while read a b c d e f
+do
+echo "       As of $a the system has been up for $c days and $e hours"
+echo ""
+echo "          #########################################################"
+echo "          ##############  HTTP ACESS FILE   #######################"
+echo "          #########################################################"
+echo "                   Last 20 lines of CURRENT ACCESS log"
+echo ""
+echo ""
+#echo $SHOW
+tail -20 /var/log/httpd/access_log
+done
