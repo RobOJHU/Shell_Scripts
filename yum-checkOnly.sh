aa62ec9b3f65faedd73478b08c9f30a2282d331f
Robert O'Steen <rosteen@jhu.edu>
2014-05-09 14:25:34 -0400

    Create yum-checkOnly.sh

---
+++ yum-checkOnly.sh
@@ -0,0 +1,1 @@
+yum check-update --exclude=kernel* --exclude=php*  --exclude=openssl* --exclude=oracle* --exclude=libgcj* --exclude=mysql*  --exclude=samba3x* --exclude=httpd* --exclude=mod_ssl* --exclude=libtdb*  --exclude=autofs* --exclude=postgresql* --exclude=rhn* --exclude=389* --exclude=java*
