915119a4e812b4341253d974162d294fd19bcd22
Robert O'Steen <rosteen@jhu.edu>
2014-05-09 14:29:01 -0400

    Create yum-downOnly.sh

---
+++ yum-downOnly.sh
@@ -0,0 +1,1 @@
+yum update  --downloadonly  --exclude=kernel* --exclude=php*  --exclude=openssl* --exclude=oracle* --exclude=libgcj* --exclude=mysql*  --exclude=samba3x* --exclude=httpd* --exclude=mod_ssl* --exclude=libtdb*  --exclude=autofs* --exclude=postgresql* --exclude=rhn* --exclude=389* --exclude=java*
