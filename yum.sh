eb8bdb24e89744a5eff649dc23c2faced1a51cba
Robert O'Steen <rosteen@jhu.edu>
2014-06-13 11:14:27 -0400

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yum update --exclude=kernel* --exclude=php*  --exclude=openssl* --exclude=oracle* --exclude=libgcj* --exclude=mysql*  --exclude=samba3x* --exclude=httpd* --exclude=mod_ssl* --exclude=libtdb*  --exclude=autofs* --exclude=postgresql* --exclude=rhn* --exclude=389* --exclude=java*
+yum update --exclude=kernel* --exclude=php*  --exclude=oracle* --exclude=libgcj* --exclude=mysql*  --exclude=samba3x* --exclude=httpd* --exclude=mod_ssl* --exclude=libtdb*  --exclude=autofs* --exclude=postgresql* --exclude=rhn* --exclude=389* --exclude=java*
