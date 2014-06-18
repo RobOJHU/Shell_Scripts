2ff43ef7bccafedb6dd7f6e1532a3965f782c41e
Robert O'Steen <rosteen@jhu.edu>
2014-06-18 14:54:23 -0400

    Create yum-yes.sh

---
+++ yum-yes.sh
@@ -0,0 +1,1 @@
+yum update -y --exclude=kernel* --exclude=php*  --exclude=oracle* --exclude=libgcj* --exclude=mysql*  --exclude=samba3x* --exclude=httpd* --exclude=mod_ssl* --exclude=libtdb*  --exclude=autofs* --exclude=postgresql* --exclude=rhn* --exclude=389* --exclude=java*
