fa51114a727b6a8ae9162baa9a5ba57cd3322f60
Robert O'Steen <rosteen@jhu.edu>
2015-04-14 14:18:31 -0400

    Update delete_timestamp_files.sh

--- delete_timestamp_files.sh
+++ delete_timestamp_files.sh
@@ -4,6 +4,7 @@ find ./ ! -newer /tmp/timestamp  -name "*.xls" -exec rm {} \;
 find ./ ! -newer /tmp/timestamp  -name "*.xlsx" -exec rm {} \;
 find ./ ! -newer /tmp/timestamp  -name "*.bk" -exec rm {} \;
 find ./ ! -newer /tmp/timestamp  -name "*.txt" -exec rm {} \;
+find ./ ! -newer /tmp/timestamp  -name "*.TXT" -exec rm {} \;
 find ./ ! -newer /tmp/timestamp  -name "*.csv" -exec rm {} \;
 find ./ ! -newer /tmp/timestamp  -name "*.log" -exec rm {} \;
 find ./ ! -newer /tmp/timestamp  -name "*.xsd" -exec rm {} \;
