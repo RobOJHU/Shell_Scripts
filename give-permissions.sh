87617da70f8a5345325eac63543356536f1e954c
Robert O'Steen <rosteen@jhu.edu>
2015-05-11 14:21:07 -0400

    Update give-permissions.sh

--- give-permissions.sh
+++ give-permissions.sh
@@ -1,6 +1,19 @@
 #!/bin/ksh
 echo "Give me the USERS login name"
 read loginname
+# if there was a group --we wanted the user to be a member of --do it here
+#usermod -a -G fjhm $loginname
+# any way to CHECK here --RobO
+
+getent passwd $loginname  > /dev/null
+if [ $? -eq 0 ]; then
+        echo "yes the user exists"
+else
+        echo "No, the user does not exist"
+        exit
+fi
+
+
 id $loginname
 setfacl -m g:$loginname:rwx /sftpdisk/ag_orders/Merkle/Fortis_Receipt_insert
 setfacl -m g:$loginname:rwx /sftpdisk/ag_orders/Merkle
